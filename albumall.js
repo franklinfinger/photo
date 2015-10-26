21b789e7f68e193d093d0f0c596db0e1b81396ca
frank finger <franklinfinger@franks-MacBook-Pro.local>
2015-10-26 14:17:18 -0400

    moving forward

---
+++ albumall.js
@@ -0,0 +1,47 @@
+var albumall = [
+
+{
+  tag: "The Wife",
+  photo1:"https://scontent-iad3-1.xx.fbcdn.net/hphotos-xtl1/v/t1.0-9/10431568_10152083723300723_4449179626595469853_n.jpg?oh=e6f8dde8f9c1da3aad11b134aac15fe8&oe=56C98C72",
+  photo2:
+"https://scontent-lga3-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/307902_10150476288379348_1423154089_n.jpg?oh=f47c69421ba814f448c4fc6ed5d9a1bc&oe=56D205CC",
+  photo3:"https://scontent-iad3-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/1441440_10201281352809964_733127429_n.jpg?oh=2de80960bb2469b8067ec115b8a2437c&oe=56C26E16",
+  name:".love"
+},
+{
+  tag: "Family",
+  photo1:"https://scontent-iad3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11692549_10203019787295948_8324741266108720195_n.jpg?oh=042d07e8c0329d570d759a135aafc8ed&oe=5686503E",
+  photo2:"https://scontent-mia1-1.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/296567_10150476524344348_1196112695_n.jpg?oh=95bdf6c212255ab4609c7673d6792c94&oe=56B55D8D",
+  photo3:"https://scontent-mia1-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/316654_10150476455459348_723898572_n.jpg?oh=f7a934e3b86aa0bf180e72397e8c223e&oe=56B3D91F",
+  name:".crazyfam"
+},
+{
+  tag: "Good Friends",
+  photo1:"https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/11014611_10154179895904348_5923096325699925518_n.jpg?oh=67a70fb19ff240bfdf87def3b23bb777&oe=56BC9ACF",
+  photo2:"https://scontent-lga3-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/207566_537115898400_3907_n.jpg?oh=8ed09791a3755cbe86f1c152a722bb7a&oe=56BE2C3C",
+  photo3:"https://scontent-mia1-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/315076_10150476288684348_663189758_n.jpg?oh=2f19bc59acc44bc12c7c7894acd72d3d&oe=56C2CA54",
+  name:".bestfriends"
+},
+{
+  tag:"Vacation",
+  photo1:"https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfp1/t31.0-8/134749_10150151077899348_5696033_o.jpg",
+  photo2:"https://scontent-mia1-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/1930581_33945754347_6128_n.jpg?oh=79deff35267dee31e786e9c20585c25a&oe=56CFEA26",
+  photo3:"https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfa1/t31.0-8/218270_10150287870434348_1099203_o.jpg"
+  name:".vacafun"
+},
+
+{
+  tag:"On The Job",
+  photo1:"https://scontent-iad3-1.xx.fbcdn.net/hphotos-xaf1/t31.0-8/339557_2393315106774_121475969_o.jpg",
+  photo2:"https://scontent-lga3-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/28720_1437702950692_3159752_n.jpg?oh=f879a7e8d1e431ea224c07506879fd25&oe=56CD8539",
+  photo3:"https://scontent-lga3-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/10399390_1113673650689_6717472_n.jpg?oh=70b366c141cdd12729a4df127e15b0d9&oe=568ADE4B",
+  name:".greatjob"
+},
+{
+  tag:"Old School",
+  photo1:"https://scontent-iad3-1.xx.fbcdn.net/hphotos-frc3/v/t1.0-9/529456_10151222987409348_128706209_n.jpg?oh=f3bebce2c3befe12358c08ca28d03c3c&oe=56BEBD24",
+  photo2:"https://scontent-lga3-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/1930581_33945729347_4425_n.jpg?oh=bce14f35a7852e256669c3633b1fd823&oe=56B6DEED",
+  photo3:"https://scontent-lga3-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/1931276_56942809347_8805_n.jpg?oh=f5a9046bd09f07b18ab9e912468d4f4c&oe=568B3772",
+  name:".goodtimes"
+}
+];
