993e6d55c264f6b8926a525585c143507430fb29
frank finger <franklinfinger@franks-MacBook-Pro.local>
2015-10-27 15:50:13 -0400

    better

--- app.js
+++ app.js
@@ -1,60 +1,53 @@
 $(document).ready(function () {
 
-var allPhotos = _.template($('#landingTmpl').html());
-var photoHtml = "";
-// var justFirstPhotos = albumall.map(function(el) { return { name: el.name, photo1: el.photo1, tag: el.tag }      })
-_.each(albumall, function (currVal, idx, arr){
+  var allPhotos = _.template($('#landingTmpl').html());
+  var photoHtml = "";
+  // var justFirstPhotos = albumall.map(function(el) { return { name: el.name, photo1: el.photo1, tag: el.tag }      })
+  _.each(albumall, function (currVal, idx, arr){
 
-  photoHtml += allPhotos(currVal);
-  console.log(photoHtml);
+    photoHtml += allPhotos(currVal);
+  });
+
+  $('.container').html(photoHtml);
+
 });
-$('.container').html(photoHtml);
-
-
-
-var manyphotos = _.template($('#phototmpl').html());
-var myphotos = "";
-// var justFirstPhotos = albumall.map(function(el) { return { name: el.name, photo1: el.photo1, tag: el.tag }      })
-_.each(albumall, function (currVal, idx, arr){
-
-  myphotos += manyphotos(currVal);
-  console.log(myphotos);
-});
-$('.container').html(myphotos);
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- $('nav a').on('click', function (event) {
+  $('nav').on('click', 'a', function (event) {
      event.preventDefault();
+       var navClick = "." + $(this).attr('rel');
+    console.log(navClick);
 
-       var clickedSection = "." + $(this).attr('rel');
-     console.log(clickedSection);
-     $(clickedSection).addClass('active-section');
-       $(clickedSection).siblings('section').removeClass('active-section');
 
- });
+
+    //  $(clickedSection).addClass('active-section');
+      // $(clickedSection).siblings('section').removeClass('active-section');
+});
+
+$('.container').on('click','img',function(event) {
+  event.preventDefault();
+  globalVar = $(this);
+  console.log(this);
+  globalVar.siblings('h2').text();
+  var clickedText = globalVar.siblings('h2').text();
+  var correctAlbum = _.filter(albumall, function(element) {
+    return element.tag === clickedText;
+});
+
+  //1). Get the siblings of the clicked item `$(this).siblings()` ... and grab text of the sibling
+  //2). Store step 1, as variable. "clickedText"
+  //3). filter of albumall, and return the one where its `tag` attribute === clickedText
+ var manyphotos = _.template($('#photoTmpl').html());
+  var myphotos = "";
+  _.each(correctAlbum, function (currVal, idx, arr){
+
+    myphotos += manyphotos(currVal);
+    console.log(myphotos);
+  });
+  $('.container').html(myphotos);
+
+
+
 });
